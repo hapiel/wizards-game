7b6e7f19a7a5a3a91efa1fe6cef5be5aabcd8a1a
Hapiel <daniel@danielsiegmund.nl>
2021-06-07 21:13:52 +0200

    Change window size and scale with buttons 1-2-3

--- GlobalSettings.gd
+++ GlobalSettings.gd
@@ -7,3 +7,16 @@ export var grav_incr_amount = 1.4
 func _process(delta):
 	if Input.is_action_just_pressed("reload"):
 		get_tree().reload_current_scene()
+		
+	if Input.is_action_just_pressed("1"):
+		OS.set_window_size(Vector2(
+			ProjectSettings.get_setting("display/window/size/width"),
+			ProjectSettings.get_setting("display/window/size/height")))
+	if Input.is_action_just_pressed("2"):
+		OS.set_window_size(Vector2(
+			ProjectSettings.get_setting("display/window/size/width") * 2,
+			ProjectSettings.get_setting("display/window/size/height") * 2)) 
+	if Input.is_action_just_pressed("3"):
+		OS.set_window_size(Vector2(
+			ProjectSettings.get_setting("display/window/size/width") * 3,
+			ProjectSettings.get_setting("display/window/size/height") * 3)) 
