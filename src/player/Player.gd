02d5c1ec9f63bbf3e866490e32b0a8e8e323496a
theo-sandstorm <theo.sandstorm@gmail.com>
2021-08-01 12:34:11 +0200

    better duckslide

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -8,6 +8,10 @@ export var jump_limiter = 0.4 # release jump key to go to force * limiter speed.
 export var jump_gravity_reduct = 0.3 # reduction of gravity during jump up.
 export var inertia = 40 #originally 5
 
+export var crawl_max_speed = 60
+export var crawl_accelration = 200
+export var slide_slowdown_rate = 0.005
+
 var velocity = Vector2.ZERO
 
 var wasOnFloor = false
@@ -59,4 +63,40 @@ func update_velocity_x(dt):
 			velocity.x = 0 
 		
 		
+	pass
+
+func update_slide_velocity_x(dt):
+	# single speed
+#	velocity.x = max_speed * get_input_direction()
+	var movement_updated = false
+	# accelerating:
+	if get_input_direction() != 0:
+		#moving the direction the player wants to go
+		if sign(velocity.x) == get_input_direction() && abs(velocity.x) < crawl_max_speed:
+			velocity.x += crawl_accelration * get_input_direction() * dt
+			movement_updated = true
+			
+		elif sign(velocity.x) != get_input_direction():
+			#brake faster
+			velocity.x = lerp(velocity.x, 0, slowdown_rate)
+			if abs(velocity.x) < 30:
+				velocity.x = 0 
+			velocity.x += crawl_accelration * get_input_direction() * dt
+			movement_updated = true
+		
+		if velocity.x > max_speed:
+			velocity.x = max_speed
+		elif velocity.x < -max_speed:
+			velocity.x = -max_speed
+	if not movement_updated:
+		print(velocity.y)
+		var modified_slowdown_rate = slide_slowdown_rate
+		if velocity.y > 30:
+			modified_slowdown_rate /= 4
+		if velocity.y < 30:
+			modified_slowdown_rate *= 4
+		
+		velocity.x = lerp(velocity.x, 0, modified_slowdown_rate)
+		if abs(velocity.x) < 30:
+			velocity.x = 0 
 	pass
