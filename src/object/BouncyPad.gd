235df4a806724d924fd60f14b35e8b5d568de3cd
theo-sandstorm <theo.sandstorm@gmail.com>
2021-08-28 14:01:48 +0200

    bouncy fun times

---
+++ src/object/BouncyPad.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var jump_boost = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TopChecker_body_entered(body):
+	
+	if body.is_in_group("player"):
+		body.get_node("StateMachine").transition_to("Air", {do_jump = true})
+		body.velocity.y -= jump_boost
+		print("BOUNCE!")
