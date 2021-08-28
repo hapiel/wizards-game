739d0f43c645d2d58f83eb388187b8c50b852809
theo-sandstorm <theo.sandstorm@gmail.com>
2021-08-28 18:45:54 +0200

    some buttons

---
+++ src/misc/mainMenuButtons.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Button_time_trial_pressed():
+	get_tree().change_scene("res://src/Levels/timeTrial2.tscn")
+
+
+func _on_Button_jump_challenge_pressed():
+	get_tree().change_scene("res://src/Levels/JumpChallenge.tscn")
+
+
+func _on_Button_exit_pressed():
+	get_tree().quit()
