0342739fc56806b0b63f7da1d649c94f3bd51cb8
theo-sandstorm <theo.sandstorm@gmail.com>
2021-09-22 14:38:23 +0200

    more moving plattforms

--- src/object/MovingPlattformParent.gd
+++ src/object/MovingPlattformParent.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var wait_duration = 0.0 # Wait is currently borked
-export var move_to = Vector2.UP * 48
+export var move_to = Vector2.UP * 4
 export var duration = 3.0
 
 onready var tween = $Tween
@@ -9,6 +9,7 @@ onready var platform = $KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	move_to = move_to * 12
 	tween.interpolate_property(platform, "position", Vector2.ZERO, move_to, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, wait_duration)
 	tween.interpolate_property(platform, "position", move_to, Vector2.ZERO, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, duration + wait_duration*2)
 	tween.start()
